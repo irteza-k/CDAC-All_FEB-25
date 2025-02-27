0346a981f4fbbdaf054be9ea30bcd3310259b226
irteza-k <85066827+irteza-k@users.noreply.github.com>
2025-02-27 10:23:37 +0530

    Delete Assignmnet_1 directory

--- Assignmnet_1/Q5SquareArea.java
+++
@@ -1,8 +0,0 @@
-public class Q5SquareArea {
-    public static void main(String[] args) {
-        int side = 4; 
-        
-        int area = side * side;
-        System.out.println("Area of the square: " + area);
-    }
-}
--- Assignmnet_1/Q6RectangleArea.java
+++
@@ -1,9 +0,0 @@
-public class Q6RectangleArea {
-    public static void main(String[] args) {
-        int length = 6;  
-        int width = 3;   
-        
-        int area = length * width;
-        System.out.println("Area of the rectangle: " + area);
-    }
-}
