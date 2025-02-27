58bd21f71c9f235d062b6fd0e7d6a63f087c0556
irteza-k <irteza0007@gmail.com>
2025-02-27 10:19:33 +0530

    Initial commit

---
+++ Assignmnet_1/Q2NegativeCheck.java
@@ -0,0 +1,11 @@
+public class Q2NegativeCheck {
+    public static void main(String[] args) {
+        int number = -5; 
+        
+        if (number < 0) {
+            System.out.println(number + " is a negative number.");
+        } else {
+            System.out.println(number + " is not a negative number.");
+        }
+    }
+}
---
+++ Assignmnet_1/Q6RectangleArea.java
@@ -0,0 +1,9 @@
+public class Q6RectangleArea {
+    public static void main(String[] args) {
+        int length = 6;  
+        int width = 3;   
+        
+        int area = length * width;
+        System.out.println("Area of the rectangle: " + area);
+    }
+}
