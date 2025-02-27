6f64d44e046bf295e54b841df2dd502abe7d2b0f
irteza-k <irteza0007@gmail.com>
2025-02-27 10:24:35 +0530

    Initial commit

---
+++ Assignments_1/Q5SquareArea.java
@@ -0,0 +1,8 @@
+public class Q5SquareArea {
+    public static void main(String[] args) {
+        int side = 4; 
+        
+        int area = side * side;
+        System.out.println("Area of the square: " + area);
+    }
+}
